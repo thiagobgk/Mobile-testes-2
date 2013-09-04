d8b865983efa9154358b7f3fc7608755fe96e82e
thiago brito <thiago@bgkgroup.com.br>
2013-09-04 13:18:54 -0300

    Css Adjustments / IOS 7 Blur Effect - Config Panel

--- js/functions.js
+++ js/functions.js
@@ -5,21 +5,24 @@
 $(document).ready(function(){
 	$('a.icon-config').click(function(){
 		
-		var element = $('div.wrap-body section.content').html();
-		var canvas = $('canvas');
+		var element = $('div.wrap-body').html();
+		//var canvas = $('canvas');
 		
-		$('div.wrap-film').fadeIn(250),
-		$('div.wrap-film div.content-config').animate({left: '0'},450);
-		$('div.people-list svg image').fadeOut(function(){
-			$('div.people-list svg image').attr('style','filter:url(#blur-effect-1)'),
-			$('div.people-list svg image').fadeIn(450)
+		$('div.wrap-film').fadeIn(250,function(){
+		 	
+			$('div.wrap-film div.content-config').animate({left: '0'},450);
+			$('div.wrap-film div.fundo-blur').animate({left: '0'},450);
+			$('div.content-blur').append(element);
+			$('div.wrap-film div.film').fadeIn(450);
 		})
+
 		
 		
 	}),
 	$('a.icon-back').click(function(){
 		$('div.wrap-film div.content-config').animate({left: '-64%'},450),
 		$('div.wrap-film').fadeOut(250),
+		$('div.content-blur').empty();
 		$('div.people-list svg image').attr('style','');
 	})
 })
